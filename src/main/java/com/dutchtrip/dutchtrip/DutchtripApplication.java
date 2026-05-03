0a2dc730b3a520184d96ef6a076b30243fadad45
young <coucoo7809@youngui-Macmini.local>
2026-05-03 17:44:48 +0900

    chore: Spring Boot 프로젝트 초기 세팅

---
+++ src/main/java/com/dutchtrip/dutchtrip/DutchtripApplication.java
@@ -0,0 +1,13 @@
+package com.dutchtrip.dutchtrip;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DutchtripApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DutchtripApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dutchtrip/dutchtrip/DutchtripApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dutchtrip.dutchtrip;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DutchtripApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
